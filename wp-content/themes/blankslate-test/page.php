84c41bfbba12fdb674d5bb764146bdc4262f477d
Joe Unander <joe@joeunander.com>
2013-10-18 13:25:42 -0400

    Page test - spacing edit, entire page

--- wp-content/themes/blankslate-test/page.php
+++ wp-content/themes/blankslate-test/page.php
@@ -3,43 +3,44 @@
  * Page template
  *
  */
- 
-  get_header(); ?>
-  
-  <div id="content" class="hfeed">
-  
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+get_header();
+?>
 
-              <div <?php post_class(); ?>>
-                  <h1 class="entry-title"><?php the_title(); ?></h1>
-                  <div class="entry-content"><?php the_content(); ?></div>
-              </div><!--hentry-->
+<div id="content" class="hfeed">
 
-          <?php endwhile;
-      endif;
-      ?>
-  
-  </div><!--#content-->
-  
-  <?php global $wp_query;
-  $post = $wp_query->post;
-  $postID = $post->ID;
-  $parent = $post->post_parent;
-  
-  //add the about-page sidebar to just the about page and any child about page
-  if ($postID == "49" || $parent == "49")
-  {
-	  echo '<div id="sidebar" class="sidebar widget-area">';
-	   
-		$args = array(
-					  'theme_location' => 'aboutmenu',
-					  'container' => 'div',
-					  'container_id' => 'about-menu',
-					  'menu_class' => 'sidbar-nav'
-				  );
-	    echo '<h2 class="sidebar-title">About</h2>';
-		wp_nav_menu( $args );
-	  echo '</div>';
-  }
-  
-  get_footer();
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <div <?php post_class(); ?>>
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+                <div class="entry-content"><?php the_content(); ?></div>
+            </div><!--hentry-->
+
+        <?php
+        endwhile;
+    endif;
+    ?>
+
+</div><!--#content-->
+
+<?php
+global $wp_query;
+$post = $wp_query->post;
+$postID = $post->ID;
+$parent = $post->post_parent;
+
+//add the about-page sidebar to just the about page and any child about page
+if ($postID == "49" || $parent == "49") {
+    echo '<div id="sidebar" class="sidebar widget-area">';
+
+    $args = array(
+        'theme_location' => 'aboutmenu',
+        'container' => 'div',
+        'container_id' => 'about-menu',
+        'menu_class' => 'sidbar-nav'
+    );
+    echo '<h2 class="sidebar-title">About</h2>';
+    wp_nav_menu($args);
+    echo '</div>';
+}
+
+get_footer();
