abaa08c9a62bd8f87decf3acbaedafb6b91fe23a
Joe Unander <joe@joeunander.com>
2013-10-18 13:24:34 -0400

    Page test - spacing edit

--- wp-content/themes/blankslate-test/page.php
+++ wp-content/themes/blankslate-test/page.php
@@ -8,15 +8,16 @@
   
   <div id="content" class="hfeed">
   
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  
-  			<div <?php post_class(); ?>>
-            	<h1 class="entry-title"><?php the_title(); ?></h1>
-                <div class="entry-content"><?php the_content(); ?></div>
-            </div><!--hentry-->
-  
-  <?php endwhile;
-  		endif; ?>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+              <div <?php post_class(); ?>>
+                  <h1 class="entry-title"><?php the_title(); ?></h1>
+                  <div class="entry-content"><?php the_content(); ?></div>
+              </div><!--hentry-->
+
+          <?php endwhile;
+      endif;
+      ?>
   
   </div><!--#content-->
   
